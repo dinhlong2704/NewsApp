54b939b4a6cf1e26459ca33af647e82f62fdefb8
RUNSYSTEM\longdp <longdp@runsystem.net>
2025-04-21 11:39:25 +0700

    Retrofit Setup

---
+++ app/src/main/java/com/example/newsapp/api/RetrofitInstance.kt
@@ -0,0 +1,32 @@
+package com.example.newsapp.api
+
+import com.example.newsapp.util.Constants.Companion.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+    companion object {
+        private val retrofit by lazy {
+            // Cấu hình logging để debug
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            // Tạo HTTP client với interceptor
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+            // Khởi tạo Retrofit
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create()) // Chuyển đổi JSON → Kotlin
+                .client(client)
+                .build()
+        }
+        // API service
+        val api by lazy {
+            retrofit.create(NewsAPI::class.java)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "653ffdd7570449fd9248c77bfd7739c8"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
