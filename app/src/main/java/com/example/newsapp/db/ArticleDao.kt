10318f0871a51416d3e3ae78288b737282918d1e
RUNSYSTEM\longdp <longdp@runsystem.net>
2025-04-21 15:04:08 +0700

    DAO and Article Setup Room

---
+++ app/src/main/java/com/example/newsapp/db/ArticleDao.kt
@@ -0,0 +1,22 @@
+package com.example.newsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.newsapp.model.Article
+
+@Dao
+interface ArticleDao {
+    // 1. Hàm thêm/cập nhật bài báo
+    @Insert(onConflict = OnConflictStrategy.REPLACE) // Nếu bài báo đã tồn tại, ghi đè lên.
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>> // cập nhật UI khi dữ liệu thay đổi dùng liveData
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
