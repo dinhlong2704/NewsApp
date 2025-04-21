1d669d9fff269d675ebc349b91730e0986d7df29
RUNSYSTEM\longdp <longdp@runsystem.net>
2025-04-21 15:07:35 +0700

    chore insert model package

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "653ffdd7570449fd9248c77bfd7739c8"
         const val BASE_URL = "https://newsapi.org/"
+        const val DATABASE_NAME = "article_db.db"
     }
 }
